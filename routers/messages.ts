cbb2c30ab2c8977ce4f318dffa35f531d6d4faa1
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-12-04 11:24:52 -0600

    Get request is done. All messages are displayed.

--- routers/messages.ts
+++ routers/messages.ts
@@ -1,8 +1,27 @@
 import express from "express";
 import {promises as fs} from "fs";
+import {Message} from "../types";
 
 const messagesRouter = express.Router();
 const path = './messages';
+
+messagesRouter.get('/', async (req, res) => {
+    await fs.mkdir(path, {recursive: true});
+
+    const files = await fs.readdir(path).catch((err) => {
+        throw err;
+    });
+
+    const allMessages: Message[] = [];
+
+    for (const file of files) {
+        const filePath = path + '/' + file;
+        const message = await fs.readFile(filePath);
+        allMessages.push(JSON.parse(message.toString()));
+    }
+
+    res.send(allMessages);
+});
 
 messagesRouter.post('/', async (req, res) => {
     await fs.mkdir(path, {recursive: true});
